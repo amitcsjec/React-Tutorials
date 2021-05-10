df0244801f3a93d45d340e7829673690cb470a16
Amit Kumar <59612091+amitcsjec@users.noreply.github.com>
2021-05-10 20:23:20 +0530

    React Components (First commit)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Heading() {
-  return <h1>My Favourite Foods</h1>;
-}
-
-ReactDOM.render(
-  <div>
-    <Heading />
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
